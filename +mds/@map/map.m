f1750c3e36ae20c42a6122b3765992a5a407757f
Carl Case <cbcase@gmail.com>
2012-03-04 15:39:08 -0800

    remove, apply, rehashing

--- +mds/@map/map.m
+++ +mds/@map/map.m
@@ -5,14 +5,17 @@ classdef map < handle
     % hash function for it (probably a static method of the class). If not,
     % then this map can hash any built in Matlab type.
     
-    properties (Constant)
+    properties (Constant, GetAccess = protected)
         DefaultNumBuckets = 100;
+        ResizeLoadFactor = 2;
+        ResizeGrowthFactor = 4;
     end
     
-    properties
+    properties (Access = protected)
         defaultSHAHasher % util.SHAHasher. Might be empty if not needed.
         hashFn % Function handle key -> 20x1 int8 array.
-        buckets
+        buckets % Cell array: each is a bucket (array of structs)
+        numElems % Total number of elements in the table
     end
     
     methods
@@ -28,8 +31,19 @@ classdef map < handle
         
         % Dispatches a get() from indexing.
         function v = subsref(this, I)
-            this.checkSubs(I);
-            v = this.get(I.subs{1});
+            switch(I(1).type)
+                case '.'
+                    % This sucks, basically. Overriding `subsref` overloads
+                    % ., (), and {} jointly. So to get regular method call
+                    % to work, we need to forward on to the builtin version
+                    % which does the right thing.
+                    v = builtin('subsref', this, I);
+                case '()'
+                    this.checkSubs(I);
+                    v = this.get(I.subs{1});
+                case '{}'
+                    error('Indexing by {} not supported.');
+            end
         end
         
         % Dispatches a set() from indexing.
@@ -38,6 +52,41 @@ classdef map < handle
             this.set(I.subs{1}, B);
         end
         
+        % Removes a key/val pair from the map. Returns true iff key was
+        % actually present (and then removed).
+        function wasPresent = remove(this, key)
+            b = this.bucketForKey(key);
+            [~, idx] = this.findInBucket(b, key);
+            if (idx > 0)
+                this.buckets{b}(idx) = [];
+                this.numElems = this.numElems - 1;
+                wasPresent = true;
+            else
+                wasPresent = false;
+            end
+        end
+        
+        % Applies func to the val for key. If key is not present, then
+        % default behavior is to no-op an return []. If optionalDefaultVal
+        % is provided, then instead a new entry mapping key to
+        % func(optionalDefaultVal) is created. A natural use is, say, a map
+        % of counts of keys. Then func can by @(x)(x+1) and the optional
+        % default val is 0.
+        function newVal = apply(this, key, func, optionalDefaultVal)
+            b = this.bucketForKey(key);
+            [~, idx] = this.findInBucket(b, key);
+            if (idx > 0)
+                this.buckets{b}(idx).val = func(this.buckets{b}(idx).val);
+                newVal = this.buckets{b}(idx).val;
+            else
+                if (exist('optionalDefaultVal', 'var'))
+                    newVal = func(optionalDefaultVal);
+                    this.insertNewRecord(b, key, newVal);
+                else
+                    newVal = [];
+                end
+            end
+        end
     end
     
     methods (Access = protected)
@@ -50,16 +99,37 @@ classdef map < handle
         end
         
         function set(this, key, val)
+            if (this.numElems / length(this.buckets) > this.ResizeLoadFactor)
+                this.Rehash();
+            end
+            
             b = this.bucketForKey(key);
             [~, idx] = this.findInBucket(b, key);
             if (idx > 0)
                 this.buckets{b}(idx).val = val;
             else
-                % Insert new record
-                rec = struct('key', key, 'val', val);
-                this.buckets{b} = [this.buckets{b} rec];
+                this.insertNewRecord(b, key, val);
             end
+        end
+        
+        function insertNewRecord(this, b, key, val)
+            rec = struct('key', key, 'val', val);
+            this.buckets{b} = [this.buckets{b} rec];
+            this.numElems = this.numElems + 1;
+        end
+        
+        function Rehash(this)
+            n = length(this.buckets);
+            oldBuckets = this.buckets;
+            this.buckets = cell(this.ResizeGrowthFactor * n, 1);
             
+            for i = 1:n
+                bucket = oldBuckets{b};
+                for j = 1:length(bucket)
+                    b = bucketForKey(bucket(j).key);
+                    this.buckets{b} = [this.buckets{b} bucket(j)];
+                end
+            end
         end
         
         function [val, idx] = findInBucket(this, b, key)
@@ -73,7 +143,7 @@ classdef map < handle
             end
             val = [];
             idx = 0;
-        end       
+        end
         
         function b = bucketForKey(this, key)
             k = length(this.buckets);
